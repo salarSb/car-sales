f67a1d6a6d2988ae447cfd4119bac068b9c6a214
salar <salarsabeti88@gmail.com>
2024-03-18 17:22:12 +0330

    add test handlers and routers

--- src/api/api.go
+++ src/api/api.go
@@ -11,9 +11,17 @@ func InitServer() {
 	cfg := config.GetConfig()
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
-	v1 := r.Group("/api/v1/")
+	api := r.Group("api")
+	v1 := api.Group("/v1/")
 	{
 		health := v1.Group("/health")
+		testRouter := v1.Group("/test")
+		routers.Health(health)
+		routers.TestRouter(testRouter)
+	}
+	v2 := api.Group("/v2/")
+	{
+		health := v2.Group("/health")
 		routers.Health(health)
 	}
 	r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
