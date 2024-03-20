6f6143eef738188bf8315b0aa8c0fab40f988c12
salar <salarsabeti88@gmail.com>
2024-03-20 16:08:38 +0330

    add iranian mobile custom validation

--- src/api/api.go
+++ src/api/api.go
@@ -3,13 +3,25 @@ package api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"github.com/salarSb/car-sales/api/routers"
+	"github.com/salarSb/car-sales/api/validations"
 	"github.com/salarSb/car-sales/config"
+	"log"
 )
 
 func InitServer() {
 	cfg := config.GetConfig()
 	r := gin.New()
+	val, ok := binding.Validator.Engine().(*validator.Validate)
+	if ok {
+		err := val.RegisterValidation("mobile", validations.IranianMobileNumberValidator, true)
+		if err != nil {
+			log.Fatal("Error on registering custom validations")
+			return
+		}
+	}
 	r.Use(gin.Logger(), gin.Recovery())
 	api := r.Group("api")
 	v1 := api.Group("/v1/")
@@ -24,5 +36,9 @@ func InitServer() {
 		health := v2.Group("/health")
 		routers.Health(health)
 	}
-	r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
+	err := r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
+	if err != nil {
+		log.Fatal("error on running router")
+		return
+	}
 }
