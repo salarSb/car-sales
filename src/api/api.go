742f5e9b6235b18bb8e7dcb436ba17f501e22d54
salar <salarsabeti88@gmail.com>
2024-03-21 23:06:29 +0330

    add swagger to project

--- src/api/api.go
+++ src/api/api.go
@@ -8,6 +8,9 @@ import (
 	"github.com/salarSb/car-sales/api/middlewares"
 	"github.com/salarSb/car-sales/api/validations"
 	"github.com/salarSb/car-sales/config"
+	"github.com/salarSb/car-sales/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 )
 
@@ -17,6 +20,7 @@ func InitServer(cfg *config.Config) {
 	r.Use(middlewares.Cors(cfg))
 	r.Use(gin.Logger(), gin.Recovery(), middlewares.LimitByRequestMiddleware())
 	RegisterRoutes(r, cfg)
+	RegisterSwagger(r, cfg)
 	err := r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
 	if err != nil {
 		log.Fatal("error on running router")
@@ -42,4 +46,14 @@ func RegisterValidators() {
 			return
 		}
 	}
+}
+
+func RegisterSwagger(r *gin.Engine, cfg *config.Config) {
+	docs.SwaggerInfo.Title = "golang web api"
+	docs.SwaggerInfo.Description = "golang web api for car sale project"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/api"
+	docs.SwaggerInfo.Host = fmt.Sprintf("localhost:%s", cfg.Server.Port)
+	docs.SwaggerInfo.Schemes = []string{"http"}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
