2b9899a38303d0b15ec317bb48ee818f1574f8c3
salar <salarsabeti88@gmail.com>
2024-05-20 17:55:28 +0330

    use singleton pattern for postgres and redis

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -6,23 +6,13 @@ import (
 	"github.com/salarSb/car-sales/data/cache"
 	"github.com/salarSb/car-sales/data/db"
 	"github.com/salarSb/car-sales/data/db/migrations"
-	"github.com/salarSb/car-sales/pkg/logging"
 )
 
 func main() {
 	cfg := config.GetConfig()
-	logger := logging.NewLogger(cfg)
-	err := cache.InitRedis(cfg)
-	if err != nil {
-		logger.Fatal(logging.Redis, logging.StartUp, err.Error(), nil)
-		return
-	}
+	cache.GetRedis()
 	defer cache.CloseRedis()
-	err = db.InitDb(cfg)
-	if err != nil {
-		logger.Fatal(logging.Postgres, logging.StartUp, err.Error(), nil)
-		return
-	}
+	db.GetDb()
 	migrations.Up1()
 	defer db.CloseDb()
 	api.InitServer(cfg)
